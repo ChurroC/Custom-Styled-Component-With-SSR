864a52761f16cffc968a52bdbf5664734850eba9
ChurroC <charanvenkatchandran@gmail.com>
2024-05-20 01:32:08 -0500

    went crazy and just built the main tagged function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-import { CustomWrapper } from "./Components/TestCustom";
+import { CustomWrapper, styledDiv } from "./Components/TestCustom";
 import { StyledWrapper } from "./Components/TestStyled";
+
+const CustomDiv = styledDiv<{
+    backgroundColor?: string;
+}>`
+:scope {
+    display: flex;
+    height: 50vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+    background-color: ${props => props.backgroundColor || "white"};
+}
+`;
 
 function App() {
     return (
@@ -18,6 +32,13 @@ function App() {
                 <button>Pink</button>
                 <button>System</button>
             </CustomWrapper>
+            <CustomDiv backgroundColor="blue">
+                <div>Theme: Blah Blah Blah</div>
+                <button>Dark</button>
+                <button>Light</button>
+                <button>Pink</button>
+                <button>System</button>
+            </CustomDiv>
         </>
     );
 }
--- src/Components/TestCustom.tsx
+++ src/Components/TestCustom.tsx
@@ -1,3 +1,53 @@
+// Goal
+// const StyledWrapper = styled.div<{ backgroundColor?: string }>`
+//     display: flex;
+//     height: 50vh;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+//     gap: 0.75rem;
+//     background-color: ${props => props.backgroundColor || "white"};
+// `;
+
+// Tagged thingamajig with string literals
+export function styledDiv<PropTypes>(
+    strings: TemplateStringsArray,
+    ...values: {
+        (
+            props: PropTypes & {
+                children: React.ReactNode;
+            }
+        ): string;
+    }[]
+) {
+    return function (
+        props: PropTypes & {
+            children: React.ReactNode;
+        }
+    ) {
+        let result = "";
+        for (let i = 0; i < strings.length; i++) {
+            result += strings[i];
+            if (i < values.length) {
+                result += values[i](props);
+            }
+        }
+
+        return (
+            <div>
+                <style>
+                    {`
+                        @scope {
+                            ${result}
+                        }
+                    `}
+                </style>
+                {props.children}
+            </div>
+        );
+    };
+}
+
 export function CustomWrapper({
     children,
     ...props
